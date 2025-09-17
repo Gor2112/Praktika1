bcc89b06a429172c047b009aa6a8bf9cc680bd65
Gor2112 <gooooooorgev@gmail.com>
2025-09-17 22:41:59 +0300

    Update c++.cpp

--- c++.cpp
+++ c++.cpp
@@ -1,26 +1,9 @@
-#include <iostream>
 #include <vector>
-using namespace std;
+#include <string>
 
-int main() {
-    // Мой список
-    vector<int> числа;
-    числа.push_back(10);
-    числа.push_back(20);
-    числа.push_back(30);
-    
-    cout << "Мой список: ";
-    for (int i = 0; i < числа.size(); i++) {
-        cout << числа[i] << " ";
-    }
-    cout << endl;
-    
-    // Добавляю число
-    числа.push_back(40);
-    cout << "Добавил 40: ";
-    for (int i = 0; i < числа.size(); i++) {
-        cout << числа[i] << " ";
-    }
-    
-    return 0;
-}
+std::vector<std::string> vec;
+vec.push_back("a");
+vec.push_back("b");
+vec.push_back("c");
+std::string first = vec[0];
+vec.pop_back();
