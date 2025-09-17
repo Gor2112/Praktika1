5c7a911e6a5fea7c9852e417b6709be841602384
Gor2112 <gooooooorgev@gmail.com>
2025-09-17 17:20:08 +0300

    Create python.py

---
+++ python.py
@@ -0,0 +1,15 @@
+# Создаем список
+numbers = [1, 2, 3, 4, 5]
+print("Список:", numbers)
+
+# Добавляем элемент
+numbers.append(6)
+print("После добавления 6:", numbers)
+
+# Удаляем элемент
+numbers.remove(3)
+print("После удаления 3:", numbers)
+
+# Перебираем элементы
+for num in numbers:
+    print("Элемент:", num)
