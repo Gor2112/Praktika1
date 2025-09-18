ad31f2f0a7f3c0a3a9fc4cb2d070b1ffed46a1fe
Gor2112 <gooooooorgev@gmail.com>
2025-09-18 13:29:13 +0300

    Update c++STACK.cpp

--- c++STACK.cpp
+++ c++STACK.cpp
@@ -4,11 +4,11 @@ using namespace std;
 
 int main() {
     stack<string> st;
-    st.push("Hello");
-    st.push("World");
-    st.push("!");
-    cout << st.top() << endl;
+    string x = "x", y = "y", z = "z";
+    st.push(x);
+    st.push(y);
+    st.push(z);
+    string top = st.top();
     st.pop();
-    cout << st.top() << endl;
-    return 0;
+    cout << top;
 }
